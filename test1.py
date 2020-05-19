2a3563072e14d0cbea55c6e5853053c4b4a6bb0a
Jiajun Li <jmsxiaoli@gmail.com>
2020-05-19 16:15:57 +0300

    Initial commit

---
+++ test1.py
@@ -0,0 +1,18 @@
+
+from itertools import product
+from math import prod
+from multiprocessing import process
+from multiprocessing.spawn import prepare
+
+from torch import random
+
+print("sdfgsdfg")
+
+print()
+
+prepare(product())
+random
+
+prepare
+
+random()
